26b9c1d2cd1ed91ee497295de603694f6b9ffdb6
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-07 11:34:35 +0600

    firebase setup

--- src/_steps.js
+++ src/_steps.js
@@ -1,12 +1,13 @@
 /*
-* steps to use firebase 
+        // ===*** steps to use firebase ***=== //
+
 1 * create a project on firebase on console.firebase.google.com 
 2 * install firebase --> npm install firebase
 3* Register web app in firebase 
 4* copy firebase init with config firebase project settings into a file firebase.init.js 
 5* export defult firebase app
-6* import app firbase.init.js into your app.js
-7* import getAuth from firebase/auth and create auth = getAuth(app)
+6* import getAuth from firebase/auth and create const auth = getAuth(app) in App.js
+7*  import app firbase.init.js into your app.js
 8* trun on googgle authentication (firebase > authentication > enable google sign in)
 9* create google provider 
 10* sign in with pop up 
